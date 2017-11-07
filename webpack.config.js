4f179a185f06c99f93a5e98d43b2ca7ebea26497
Anton Zdanov <akumael@gmail.com>
2017-11-07 20:52:56 +0200

    Finish boilerplate of webpack, babel, hmr and html templates for future reference and this project.

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const env = process.env.NODE_ENV
+
+const plugins = [
+  new CleanWebpackPlugin(['dist']),
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+    inject: 'body'
+  }),
+  new webpack.NamedModulesPlugin(),
+  new webpack.HotModuleReplacementPlugin()
+]
+
+module.exports = {
+  entry: {
+    bundle: ['./src/js/index.js']
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use:
+          env === 'production'
+            ? ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: ['css-loader']
+            })
+            : ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  stats: 'minimal',
+  devtool: env === 'production' ? 'source-map' : 'cheap-eval-source-map',
+  plugins:
+    env === 'production'
+      ? [
+        ...plugins,
+        new ExtractTextPlugin({
+          filename: '[name].css'
+        })
+      ]
+      : plugins,
+  resolve: {
+    extensions: ['*', '.js']
+  }
+}
