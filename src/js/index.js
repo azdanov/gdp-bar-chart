29db80a86826b23548729f2eb1ad206768b2e401
Anton Zdanov <azdanov@protonmail.com>
2017-11-22 23:47:06 +0200

    Finish Chart

--- src/js/index.js
+++ src/js/index.js
@@ -1,72 +1,132 @@
 import * as d3 from 'd3';
 import '../css/index.css';
 
-const w = 500;
-const h = 300;
-const padding = 30;
+const svg = d3.select('svg');
+const margin = { top: 30, right: 30, bottom: 40, left: 60 };
+const width = +svg.attr('width') - margin.left - margin.right;
+const height = +svg.attr('height') - margin.top - margin.bottom;
 
-// Dynamic, random dataset
-const dataset = [];
-const numDataPoints = 50;
-const xRange = Math.random() * 1000;
-const yRange = Math.random() * 1000;
+const parseTime = d3.timeParse('%Y-%m-%d');
+const bisectDate = d3.bisector(d => d.year).left;
 
-for (let i = 0; i < numDataPoints; i++) {
-  const newNumber1 = Math.floor(Math.random() * xRange);
-  const newNumber2 = Math.floor(Math.random() * yRange);
-  dataset.push([newNumber1, newNumber2]);
+const x = d3.scaleTime().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
+
+const line = d3
+  .line()
+  .x(d => x(d.year))
+  .y(d => y(d.value));
+
+const g = svg
+  .append('g')
+  .attr('transform', `translate(${margin.left},${margin.top})`);
+
+function calcQuarter(monthNum) {
+  if (monthNum < 3) return 'Q1';
+  else if (monthNum < 6) return 'Q2';
+  else if (monthNum < 9) return 'Q3';
+  return 'Q4';
 }
 
-// Create scale functions
-const xScale = d3
-  .scaleLinear()
-  .domain([0 - padding, d3.max(dataset, d => d[0])])
-  .range([padding, w - padding * 2]);
+d3.json(
+  'https://cdn.rawgit.com/azdanov/gdp-bar-chart/19384513/src/data/data.json',
+  (error, json) => {
+    if (error) throw error;
 
-const yScale = d3
-  .scaleLinear()
-  .domain([0 - padding, d3.max(dataset, d => d[1])])
-  .range([h - padding, padding]);
+    const data = json.data.map(([date, quantity]) => ({
+      year: parseTime(date),
+      value: quantity,
+    }));
 
-const aScale = d3
-  .scaleSqrt()
-  .domain([0 - padding, d3.max(dataset, d => d[1])])
-  .range([0, 10]);
+    x.domain(d3.extent(data, d => d.year));
+    y.domain([0, d3.max(data, d => d.value) * 1.005]);
 
-// Define X axis
-const xAxis = d3.axisBottom(xScale).ticks(5);
+    g
+      .append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
 
-// Define Y axis
-const yAxis = d3.axisLeft(yScale).ticks(5);
+    g
+      .append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3.axisLeft(y).ticks(7, '$,.0s'));
 
-// Create SVG element
-const svg = d3
-  .select('body')
-  .append('svg')
-  .attr('width', w)
-  .attr('height', h);
+    g
+      .append('path')
+      .datum(data)
+      .attr('class', 'line')
+      .attr('d', line);
 
-// Create circles
-svg
-  .selectAll('circle')
-  .data(dataset)
-  .enter()
-  .append('circle')
-  .attr('cx', d => xScale(d[0]))
-  .attr('cy', d => yScale(d[1]))
-  .attr('r', d => aScale(d[1]))
-  .attr('fill', d => `rgb(${d[1] % 255}, ${d[1] % 255}, ${d[1] % 255})`);
+    const focus = g
+      .append('g')
+      .attr('class', 'focus')
+      .style('display', 'none');
 
-// Create X axis
-svg
-  .append('g')
-  .attr('class', 'axis')
-  .attr('transform', `translate(0,${h - padding})`)
-  .call(xAxis);
+    focus
+      .append('line')
+      .attr('class', 'x-hover-line hover-line')
+      .attr('y1', 0)
+      .attr('y2', height);
 
-// Create Y axis
-svg
-  .append('g')
-  .attr('class', 'axis')
-  .attr('transform', `translate(${padding},0)`)
-  .call(yAxis);
+    focus
+      .append('line')
+      .attr('class', 'y-hover-line hover-line')
+      .attr('x1', 0)
+      .attr('x2', width);
+
+    focus.append('circle').attr('r', 5);
+
+    const tooltip = d3
+      .select('body')
+      .append('div')
+      .attr('class', 'tooltip')
+      .style('display', 'none');
+    tooltip.append('p').attr('class', 'quantity');
+    tooltip.append('p').attr('class', 'year');
+
+    let tooltipsEnabled = false;
+
+    function disableTooltips() {
+      if (tooltipsEnabled) {
+        focus.style('display', 'none');
+        tooltip.style('display', 'none');
+      } else {
+        focus.style('display', null);
+        tooltip.style('display', 'inline');
+      }
+      tooltipsEnabled = !tooltipsEnabled;
+    }
+
+    function mouseMove() {
+      const x0 = x.invert(d3.mouse(this)[0]);
+      const i = bisectDate(data, x0, 1);
+      const d0 = data[i - 1];
+      const d1 = data[i];
+      const d = x0 - d0.year > d1.year - x0 ? d1 : d0;
+
+      // Assign text
+      focus.attr('transform', `translate(${x(d.year)}, ${y(d.value)})`);
+      tooltip.select('.quantity').text(() => `${d.value}`);
+      tooltip
+        .select('.year')
+        .text(
+          () => `${calcQuarter(d.year.getMonth())} ${d.year.getFullYear()}`,
+        );
+      tooltip
+        .style('left', `${d3.event.pageX - 30}px`)
+        .style('top', `${d3.event.pageY - 30}px`);
+      focus.select('.x-hover-line').attr('y2', height - y(d.value));
+      focus.select('.y-hover-line').attr('x2', -x(d.year));
+    }
+
+    svg
+      .append('rect')
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+      .attr('class', 'overlay')
+      .attr('width', width)
+      .attr('height', height)
+      .on('mousemove', mouseMove)
+      .on('click', disableTooltips);
+  },
+);
